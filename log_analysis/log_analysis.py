95866b3fdcac128828d3cea2ee2dce523b340a7f
Igor Oliveira <igoro@br.ibm.com>
2018-03-07 09:23:31 -0300

    Project 3 - Log Analysis re-submission

--- log_analysis/log_analysis.py
+++ log_analysis/log_analysis.py
@@ -1,47 +1,72 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
 
-def print_result(question, result):
+def print_top_articles(dbname, query):
+    """Prints out the top 3 articles of all time."""
+    db = psycopg2.connect('dbname={}'.format(dbname))
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    question = 'What are the most popular three articles of all time?'
     print(question)
-    for row in result:
-        print(row[0], row[1])
+    for row in results:
+        print(row[0], '-', row[1], 'views')
+    db.close()
+
+def print_top_authors(dbname, query):
+    """Prints a list of authors ranked by article views."""
+    db = psycopg2.connect('dbname={}'.format(dbname))
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    question = 'Who are the most popular article authors of all time?'
+    print(question)
+    for row in results:
+        print('Author:', row[0], '-', row[1], 'views')
+    db.close()
 
 
-query_1 = """
-SELECT title, count(*) AS accesses FROM articles JOIN
-log ON concat('/article/', articles.slug) = log.path
-WHERE log.status = '200 OK'
-GROUP BY log.path, articles.title ORDER BY accesses DESC limit 3;
-"""
+
+def print_errors_over_one(dbname, query):
+    """Prints out the days where more than 1% of requests were errors."""
+    db = psycopg2.connect('dbname={}'.format(dbname))
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    question = 'On which days did more than 1% of requests lead to errors?'
+    print(question)
+    for row in results:
+        print('Date:', row[0], 'Error ratio:', '{0:.2f}%'.format(row[1]))
+    db.close()
 
 
-query_2 = """
-SELECT authors.name, count(*) AS accesses FROM articles JOIN
-authors ON articles.author = authors.id join log ON
-CONCAT('/article/', articles.slug) = log.path WHERE
-log.status = '200 OK' GROUP BY authors.name ORDER BY accesses DESC;
-"""
+if __name__ == '__main__':
+    query_1 = """
+    SELECT title, count(*) AS accesses FROM articles JOIN
+    log ON concat('/article/', articles.slug) = log.path
+    WHERE log.status = '200 OK'
+    GROUP BY log.path, articles.title ORDER BY accesses DESC limit 3;
+    """
+
+    query_2 = """
+    SELECT authors.name, count(*) AS accesses FROM articles JOIN
+    authors ON articles.author = authors.id join log ON
+    CONCAT('/article/', articles.slug) = log.path WHERE
+    log.status = '200 OK' GROUP BY authors.name ORDER BY accesses DESC;
+    """
+
+    query_3 = """
+    SELECT table1.day,(table2.hits * 100.0)/(table1.hits) AS error_ratio FROM
+    ((SELECT DATE(log.time) AS day, count(*) AS hits FROM log GROUP BY day)
+    AS table1 JOIN (SELECT DATE(time) AS day, count(*) AS hits FROM log
+    WHERE status = '404 NOT FOUND' GROUP BY day) AS table2 ON
+    table1.day = table2.day) WHERE (table2.hits * 100.0)/(table1.hits) > 1;
+    """
 
 
-query_3 = """
-SELECT table1.day,(table2.hits * 100.0)/(table1.hits) AS error_ratio FROM
-((SELECT DATE(log.time) AS day, count(*) AS hits FROM log GROUP BY day)
-AS table1 JOIN (SELECT DATE(time) AS day, count(*) AS hits FROM log
-WHERE status = '404 NOT FOUND' GROUP BY day) AS table2 ON
-table1.day = table2.day) WHERE (table2.hits * 100.0)/(table1.hits) > 1;
-"""
-
-
-db = psycopg2.connect('dbname=news')
-c = db.cursor()
-c.execute(query_1)
-result1 = c.fetchall()
-c.execute(query_2)
-result2 = c.fetchall()
-c.execute(query_3)
-result3 = c.fetchall()
-
-print_result('What are the most popular three articles of all time?', result1)
-print_result('Who are the most popular article authors of all time?', result2)
-print_result('On which days did more than 1% of requests lead to errors?',
-             result3)
+    dbname = 'news'
+    print_top_articles(dbname, query_1)
+    print_top_authors(dbname, query_2)
+    print_errors_over_one(dbname, query_3)
